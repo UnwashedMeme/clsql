945ff67ce59c8be1bfdec385145a1d62fcad3352
Nathan Bird <nathan@acceleration.net>
2009-04-20 10:15:25 -0400

    new syntax macro (FILE-ENABLE-SQL-READER-SYNTAX) Turns on the SQL reader syntax for the rest of the file. The CL spec says that when finished loading a file the original *readtable* is restored.  clhs COMPILE-FILE
    This is because we kept having problems with the read syntax
    not being disabled. When clsql goes to enable the syntax it first
    checks that it hasn't already done so. Clsql believed it had, but
    the lisp had correctly disabled it and so we are left without the
    syntax being enabled.

--- sql/package.lisp
+++ sql/package.lisp
@@ -391,6 +391,7 @@
          #:locally-disable-sql-reader-syntax
          #:locally-enable-sql-reader-syntax
          #:restore-sql-reader-syntax-state
+	 #:file-enable-sql-reader-syntax
 
          ;; SQL operations (operations.lisp)
          #:sql-query
